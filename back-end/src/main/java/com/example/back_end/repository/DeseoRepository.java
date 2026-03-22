8dc3810668f2b7c6af15655b964118b40f6d1edc
JUANPIS008 <159933266+JUANPIS008@users.noreply.github.com>
2026-03-22 18:55:40 -0500

    Delete back-end directory

--- back-end/src/main/java/com/example/back_end/repository/DeseoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.back_end.repository;
-
-import com.example.back_end.model.Deseo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeseoRepository extends JpaRepository<Deseo, Long> {
-}
